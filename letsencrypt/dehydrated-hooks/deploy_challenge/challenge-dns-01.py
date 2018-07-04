82352e165c80bcd8f7440c6a0d73fe1e26358e18
Ivan Shapovalov <intelfx@intelfx.name>
2018-07-04 21:39:28 +0300

    letsencrypt: challenge-dns-01.py: write defensively, not offensively
    defensive programming is when you try to fix errors as you go,
    offensive programming is when you loudly crash into the smallest
    misconfiguration

--- letsencrypt/dehydrated-hooks/deploy_challenge/challenge-dns-01.py
+++ letsencrypt/dehydrated-hooks/deploy_challenge/challenge-dns-01.py
@@ -19,43 +19,62 @@ def wait(*, name, type, target):
 	delay = 1
 	while True:
 		try:
-			for r in dns.resolver.query(name, 'TXT'):
-				r = [ str(b, encoding='ascii') for b in r.strings ]
-				if r != [target]:
-					raise RuntimeError(f'Wrong record type {type} name {name} target {target} actual {r}')
-				return
+			answers = {
+				str(b, encoding='ascii')
+				for r in dns.resolver.query(name, 'TXT')
+				for b in r.strings
+			}
+			if answers == {target}:
+				break
+			logging.debug(f'wrong record type {type} name {name} target {target} actual {answers}')
 		except dns.resolver.NXDOMAIN as e:
+			logging.debug(f'NXDOMAIN looking for record type {type} name {name}')
 			pass
 
 		logging.info(f'will wait for {delay} seconds for record type {type} name {name} target {target} to appear')
 		time.sleep(delay)
-		delay = min(60, delay*2)
+		delay = min(300, delay*2)
+
+	logging.info(f'will wait another {delay} seconds')
+	time.sleep(delay)
 
 
-def deploy(*, domain, name, type, target):
+def find(zone, name, type, target=None):
+	for r in zone.records:
+		if (r.type == type and
+		    f'{r.name}.{zone.domain}' == name and
+		    target is None or r.target == target):
+			yield r
+
+
+def deploy(*, zone, name, type, target):
+	for r in find(zone=zone, name=name, type=type):
+		logging.info(f'will delete record id {r.id} type {r.type} name {r.name} target {r.target}')
+		r.delete()
+
 	logging.info(f'will create record type {type} name {name} target {target}')
-	domain.record_create(record_type=type, name=name, target=target, ttl_sec=60)
+	zone.record_create(record_type=type, name=name, target=target, ttl_sec=60)
 	wait(name=name, type=type, target=target)
 
 
-def clean(*, domain, name, type, target):
+def clean(*, zone, name, type, target):
 	# Read existing records
 	found = False
-	for r in domain.records:
-		if (r.type == type and
-		    f'{r.name}.{config.domain}' == name and
-		    r.target == target):
-			logging.info(f'will delete record id {r.id} type {r.type} name {r.name} target {r.target}')
-			r.delete()
-			found = True
+	for r in find(zone=zone, name=name, type=type, target=target):
+		logging.info(f'will delete record id {r.id} type {r.type} name {r.name} target {r.target}')
+		r.delete()
+		found = True
 	if not found:
-		raise RuntimeError(f'Failed to find record type {type} name {name} target {target}')
+		logging.warn(f'could not find record type {type} name {name} target {target}')
 
 actions = {
 	'deploy_challenge': deploy,
 	'clean_challenge': clean,
 }
 
+#
+# main: parse arguments
+#
 
 lib.configure_logging(prefix='DNS-01: ')
 
@@ -68,23 +87,13 @@ args = parser.parse_args()
 
 lib.configure_logging(prefix=f'DNS-01: {args.domain}: ')
 
+
 #
-# DNS: linode.com
+# load config
 #
 
 config = yaml.load(open('letsencrypt.yaml'))
 config = lib.attrdict(config)
-config = config.linode
-
-#
-# Sanity-check requested domain vs config
-#
-
-def subdomain_of(subdomain, domain):
-	return subdomain == domain or subdomain.endswith('.' + domain)
-
-if not subdomain_of(args.domain, config.domain):
-	raise RuntimeError(f'DNS-01: {args.domain}: config is for {config.domain}')
 
 
 #
@@ -96,22 +105,36 @@ try:
 	R.nameservers = list({
 		ai[4][0]
 		for ns in config.nameservers
-		for ai in socket.getaddrinfo(ns)
+		for ai in socket.getaddrinfo(ns, None)
 	})
 	dns.resolver.default_resolver = R
 except AttributeError:
+	logging.warn('using default resolver, adverse caching may occur')
 	pass
 
-linode = L.LinodeClient(config.token)
-domains = linode.domains(L.Domain.domain == config.domain)
-domains = list(domains)
 
-if len(domains) != 1:
-	raise RuntimeError(f'DNS-01: {args.domain}: Linode reports {len(domains)} domains matching {config.domain}')
+#
+# DNS: linode.com
+#
+
+def subdomain_of(subdomain, domain):
+	return subdomain == domain or subdomain.endswith('.' + domain)
+
+config = config.linode
+linode = L.LinodeClient(config.token)
+zones = [
+	d
+	for d
+	in linode.domains()
+	if subdomain_of(args.domain, d.domain)
+]
+
+if len(zones) != 1:
+	raise RuntimeError(f'DNS-01: {args.domain}: found {len(zones)} matching zones')
 
 actions[args.action](
-	domain = domains[0],
-	name=f'_acme-challenge.{args.domain}', # {config.domain} will be stripped by Linode
+	zone = zones[0],
+	name=f'_acme-challenge.{args.domain}', # zone prefix will be stripped by Linode
 	target=args.dns_token,
 	type='TXT'
 )

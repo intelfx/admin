0e695aa7f40497ea1b4eccb9b83064e669ed11c1
Ivan Shapovalov <intelfx@intelfx.name>
2021-10-21 00:26:49 +0300

    letsencrypt: dehydrated-hooks: deploy_cert_real: convert to id_ed25519 identity where possible

--- letsencrypt/dehydrated-hooks/deploy_cert_real/router.nexus.i.intelfx.name/owrt.sh
+++ letsencrypt/dehydrated-hooks/deploy_cert_real/router.nexus.i.intelfx.name/owrt.sh
@@ -3,6 +3,6 @@
 . "${BASH_SOURCE%/*}/../common.sh" || exit
 
 host="root@router.nexus.i.intelfx.name"
-identity="/etc/admin/keys/id_rsa"
+identity="/etc/admin/keys/id_ed25519"
 
 ssh_cert_to_openwrt

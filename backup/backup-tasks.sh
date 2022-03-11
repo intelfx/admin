178ce77140a225b004196eef81799f5e32a9d6c9
Ivan Shapovalov <intelfx@intelfx.name>
2022-03-11 15:25:30 +0300

    backup: backup-tasks.sh: drop privileges to operator whenever possible, do not run everything as root

--- backup/backup-tasks.sh
+++ backup/backup-tasks.sh
@@ -11,8 +11,11 @@ RC=0
 FAILED_TASKS=()
 
 run_task() {
-	if ! "$@"; then
-		err "Failed task: $*"
+	local user="$1"
+	shift
+
+	if ! runuser -u $user -- "$@"; then
+		err "Failed task: $* (as $user${capa:+ with $capa})"
 		(( ++RC ))
 		FAILED_TASKS+=( "$*" )
 	fi
@@ -28,10 +31,9 @@ log_tasks() {
 }
 trap log_tasks EXIT
 
-run_task ./backup-pull-openwrt.sh root@router.nexus.i.intelfx.name
-run_task ./backup-pull-mikrotik.sh admin@chr.nexus.i.intelfx.name
-run_task ./backup-borgbase-borg.sh
-run_task ./backup-borgbase-mirror.sh --itemize-changes
+run_task operator ./backup-pull-openwrt.sh root@router.nexus.i.intelfx.name
+run_task operator ./backup-pull-mikrotik.sh admin@chr.nexus.i.intelfx.name
+run_task root     ./backup-borgbase-borg.sh
+run_task operator ./backup-borgbase-mirror.sh --itemize-changes
 
-run_task ./macrium-consolidate.sh /mnt/data/Backups/SMB/smb-arcadia 30 60
-
+run_task operator ./macrium-consolidate.sh /mnt/data/Backups/SMB/smb-arcadia 30 60

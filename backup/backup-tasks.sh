ee2747134c398fc415748b7d73f96b32719a7bd2
Ivan Shapovalov <intelfx@intelfx.name>
2022-03-11 15:27:48 +0300

    backup: backup-tasks.sh: use setpriv instead of runuser; add ambient CAP_FOWNER to macrium-consolidate.sh to permit mtime manipulation

--- backup/backup-tasks.sh
+++ backup/backup-tasks.sh
@@ -11,10 +11,17 @@ RC=0
 FAILED_TASKS=()
 
 run_task() {
-	local user="$1"
+	local user="$1" capa
 	shift
 
-	if ! runuser -u $user -- "$@"; then
+	local setpriv_args=( --inh-caps -all )
+	if [[ $user == *,* ]]; then
+		capa="${user#*,}"; capa="${capa//CAP_}"; capa="${capa//cap_}"
+		user="${user%%,*}"
+		setpriv_args=( --inh-caps "-all,$capa" --ambient-caps "$capa" )
+	fi
+
+	if ! setpriv --reuid="$user" --regid="$user" --init-groups --reset-env "${setpriv_args[@]}" -- "$@"; then
 		err "Failed task: $* (as $user${capa:+ with $capa})"
 		(( ++RC ))
 		FAILED_TASKS+=( "$*" )
@@ -36,4 +43,4 @@ run_task operator ./backup-pull-mikrotik.sh admin@chr.nexus.i.intelfx.name
 run_task root     ./backup-borgbase-borg.sh
 run_task operator ./backup-borgbase-mirror.sh --itemize-changes
 
-run_task operator ./macrium-consolidate.sh /mnt/data/Backups/SMB/smb-arcadia 30 60
+run_task operator,+CAP_FOWNER ./macrium-consolidate.sh /mnt/data/Backups/SMB/smb-arcadia 30 60

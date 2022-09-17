cc7de38e3d9888eccbb995a066c80aa8d2dfdbd0
Ivan Shapovalov <intelfx@intelfx.name>
2022-09-17 18:00:06 +0300

    isolate: isolate-daemon.sh: refactor, do not waste time repeatedly moving tids

--- isolate/isolate-daemon.sh
+++ isolate/isolate-daemon.sh
@@ -9,9 +9,13 @@ TIMEOUT=1
 
 [[ "$CGROUP_NAME" ]] || die "Bad cgroup name (not given)"
 
+ROOT_PATH="/sys/fs/cgroup"
 CGROUP_PATH="$(realpath -qm /sys/fs/cgroup/$CGROUP_NAME)"
 
+declare -a THREADS
 declare -A FAILED_TIDS
+declare -A SEEN_TIDS
+
 
 while :; do
 	if ! [[ -d "$CGROUP_PATH" ]]; then
@@ -23,18 +27,39 @@ while :; do
 		die "Cannot set cgroup type: cgroup.type = threaded"
 	fi
 
+	SEEN_TIDS=()
+	moves=0
 	errs=0
-	while read tid; do
-		if ! echo $tid > "$CGROUP_PATH/cgroup.threads" 2>/dev/null; then
-			if ! [[ ${FAILED_TIDS[$tid]} ]]; then
-				(( ++errs ))
-			fi
-			FAILED_TIDS[$tid]=1
-		else
-			unset FAILED_TIDS[$tid]
-			log "Moved [/sys/fs/cgroup -> $CGROUP_PATH]: $tid"
+
+	readarray -t THREADS < "$ROOT_PATH/cgroup.threads"
+	for tid in "${THREADS[@]}"; do
+		SEEN_TIDS[$tid]=1
+
+		if [[ ${FAILED_TIDS[$tid]} ]]; then
+			continue
 		fi
-	done < /sys/fs/cgroup/cgroup.threads
+
+		if ! echo $tid > "$CGROUP_PATH/cgroup.threads" 2>/dev/null; then
+			err "Failed: $tid ($(</proc/$tid/comm))"
+			FAILED_TIDS[$tid]=1
+			(( ++errs ))
+		else
+			log "Moved [$ROOT_PATH -> $CGROUP_PATH]: $tid ($(</proc/$tid/comm))"
+			(( ++moves ))
+		fi
+	done
+
+	# forget non-existent tids
+	for tid in "${!FAILED_TIDS[@]}"; do
+		if ! [[ ${SEEN_TIDS[$tid]} ]]; then
+			log "Disappeared: $tid (previously failed)"
+			unset FAILED_TIDS[$tid]
+		fi
+	done
+
+	if (( moves )); then
+		log "Moved $moves new threads"
+	fi
 	if (( errs )); then
 		err "Failed to move $errs new threads"
 	fi

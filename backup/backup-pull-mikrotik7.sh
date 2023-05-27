6927096cecb2466807d817dadef5e7c8828d52a0
Ivan Shapovalov <intelfx@intelfx.name>
2023-05-27 05:56:42 +0300

    backup: backup-pull-mikrotik7.sh: reassess all hacks for ROSv7
    - use a full length RSA key
    - use new crypto primitives (corresponding to strong crypto in ROSv7.9+)
    - reenable exports

---
+++ backup/backup-pull-mikrotik7.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+cd "${BASH_SOURCE%/*}"
+. lib/lib.sh
+
+host="$1"
+dest="/mnt/data/Backups/Hosts"
+password="$(< /etc/admin/keys/backup-mikrotik )"
+identity="/etc/admin/keys/id_rsa"
+
+log "$0: backing up '$host' to '$dest'"
+
+ssh_prep_parse_host
+dest="$dest/$addr"
+known_hosts="$dest/known_hosts"
+
+ssh_prep -o PubkeyAcceptedAlgorithms=+rsa-sha2-256
+
+trap "rm -rf '$tempdir'" EXIT
+tempdir="$(mktemp -d)"
+
+do_ssh <<-EOF
+	/system backup save password="$password" name="auto-backup"
+	/export compact file="auto-backup.rsc"
+	/export verbose file="auto-backup-verbose.rsc"
+EOF
+
+do_sftp <<-EOF
+	lcd "$tempdir"
+	get auto-backup.backup
+	get auto-backup.rsc "$host_identity.rsc"
+	get auto-backup-verbose.rsc "$host_identity-verbose.rsc"
+	rm auto-backup*
+EOF
+
+mkdir -p "$dest"
+rsync -rt --chmod=ugo=rwX "$tempdir"/ "$dest"/

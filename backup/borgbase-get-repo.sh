a8b0cb5ec3a242268cef8cee91f6db4e720fff78
Ivan Shapovalov <intelfx@intelfx.name>
2023-10-17 22:25:22 +0300

    backup: borgbase-get-repo.sh: wait for the repo hostname to resolve

--- backup/borgbase-get-repo.sh
+++ backup/borgbase-get-repo.sh
@@ -12,6 +12,7 @@ cd "${BASH_SOURCE%/*}"
 
 BORGBASE_API="https://api.borgbase.com/graphql"
 BORGBASE_KEY="/etc/admin/keys/borgbase"
+BORGBASE_MAX_WAIT=300
 
 #
 # arguments
@@ -90,8 +91,40 @@ borgbase_call() {
 	cat "$response"
 }
 
+borgbase_wait() {
+	local host="$1"
+	local start_ts="$(date +%s)" now_ts
+	local timeout=1
+
+	while :; do
+		if getent hosts "$host" &>/dev/null; then
+			break
+		fi
+		now_ts="$(date +%s)"
+		if ! (( now_ts < start_ts + BORGBASE_MAX_WAIT )); then
+			err "Failed to wait for $url to resolve ($((now_ts-start_ts))s >= ${BORGBASE_MAX_WAIT}s)"
+			return 1
+		fi
+
+		log "Waiting ${timeout}s for $host to resolve..."
+		sleep "$timeout"
+		(( timeout = timeout*2 < 60 ? timeout*2 : 60 ))
+	done
+}
+
+id_to_host() {
+	echo "$1.repo.borgbase.com"
+}
+
 id_to_url() {
 	echo "$1@$1.repo.borgbase.com"
+}
+
+borgbase_respond() {
+	local id="$1"
+
+	borgbase_wait "$(id_to_host "$id")"
+	id_to_url "$id"  # prints to stdout
 }
 
 #
@@ -100,13 +133,13 @@ id_to_url() {
 
 borgbase_call "query" "{ repoList{id, name} }" | jq -r -e '.data.repoList[] | "\(.id)\t\(.name)"' | while IFS=$'\t' read id name; do
 	if [[ "$REPO_NAME" == "$name" ]]; then
-		id_to_url "$id"
+		borgbase_respond "$id"
 		exit 0
 	fi
 done
 
 borgbase_call "query" "mutation { repoAdd(name:\"$REPO_NAME\", quotaEnabled: false, alertDays: 0, $REPO_ADD_ARGS) { repoAdded{id} } }" | jq -r -e '.data.repoAdd.repoAdded.id' | while read id; do
-	id_to_url "$id"
+	borgbase_respond "$id"
 	exit 0
 done
 
